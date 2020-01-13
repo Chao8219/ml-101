3894799c6f52047c08718f4e0b25833e608f821e
Zichao Hou <zichao@gwu.edu>
2020-01-13 17:18:30 -0500

    linear regression on titanic data updated

--- linear_regression/matlab/linear_regression_titanic.m
+++ linear_regression/matlab/linear_regression_titanic.m
@@ -1,7 +1,7 @@
 % This file is to do linear regression on the titanic dataset
 % where the output would be the fare
 % inputs would be pclass, sex, age, sibsp, parch
-function linear_regression
+function linear_regression_titanic
 clc; close all;
 
 % read raw dataset
@@ -22,6 +22,15 @@ fine_DS_shuffled = fine_DS_arr(randperm(size(fine_DS_arr, 1)), :);
 % construct dataset
 X_DS = fine_DS_shuffled(:, 1:5);
 y_DS = fine_DS_shuffled(:, 6);
+
+% add polynomial features
+poly = [];
+for i = 1:5
+    for j = 1:5
+        poly = [poly, X_DS(:, i).*X_DS(:, j)];
+    end
+end
+X_DS = [X_DS, poly];
 
 n_train = size(X_DS, 2); % feature number
 m_total = size(y_DS, 1); % data number
@@ -51,7 +60,7 @@ X_train = [ones(m_train, 1), X_train];
 X_test = [ones(m_test, 1), X_test];
 
 % initialize for optimization
-theta_0 = ones(n_train+1, 1);
+theta_0 = ones(n_train+1, 1)*20.0;
 reg_lambda = 1e-2;
 [J, dJ_dtheta] = cost_func(theta_0, X_train, y_train, reg_lambda);
 
@@ -63,10 +72,22 @@ fminunc_options = optimoptions('fminunc', ...
 % find the optimal theta
 [theta_opt, J] = fminunc(func_handle, theta_0, fminunc_options);
 
-% exam the theta_opt
-y_predict = X_test * theta_opt;
-fprintf('Train Accuracy: %f\n', mean(double(y_predict == y_test)) * 100);
+theta_opt_normal = normal_eq(X_train, y_train, reg_lambda);
+fprintf('The relative difference between two methods: %.2e\n', ...
+    norm(theta_opt_normal-theta_opt)./norm(theta_opt_normal+theta_opt));
 
+% exam the theta_opt
+y_predict = X_train * theta_opt;
+err_val = norm(y_predict-y_train) ./ norm(y_predict+y_train);
+fprintf('Train Error: %.2e\n', err_val);
+
+end
+
+function theta_opt = normal_eq(X_train, y_train, reg_lambda)
+idm = eye(size(X_train, 2));
+idm(1, 1) = 0.0;
+temp_mat = X_train' * X_train + reg_lambda * idm;
+theta_opt = inv(temp_mat) * X_train' * y_train;
 end
 
 function [J, dJ_dtheta] = cost_func(theta, X_train, y_train, reg_lambda)
